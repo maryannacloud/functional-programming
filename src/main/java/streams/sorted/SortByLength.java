1ec1723ca61a6182585d2b40985c7b7002edc6c0
marynanesterenko <marynanesterenko.qa@gmail.com>
2025-03-16 01:55:26 -0400

    Improved sortByLength() method by using Comparator instead of passing Lambda Expression to .sorted() method

--- src/main/java/streams/sorted/SortByLength.java
+++ src/main/java/streams/sorted/SortByLength.java
@@ -1,5 +1,6 @@
 package streams.sorted;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,12 +8,12 @@ public class SortByLength {
 
     public static List<String> sortByLength(List<String> strings) {
         return strings.stream()
-                .sorted((s1, s2) -> Integer.compare(s1.length(), s2.length()))
+                .sorted(Comparator.comparingInt(String::length))
                 .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
         List<String> words = List.of("apple", "banana", "kiwi", "strawberry");
-        System.out.println(sortByLength(words)); // Output: [kiwi, apple, banana, strawberry]
+        System.out.println(sortByLength(words));
     }
 }
